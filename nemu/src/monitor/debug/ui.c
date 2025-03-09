fcbefdf4ccccf036e58721f59620a7e67d6b1a7a
tracer-ics2017 <tracer@njuics.org>
2025-03-10 00:36:16 +0800

    >  compile 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:36:16 up 7 min,  1 user,  load average: 0.02, 0.14, 0.09 df7a1a69c6e13cd1d83a21e547f1156d5ff2088

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -46,8 +46,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si", "Let the program step N instructions and then pause execution. If N is not given, the default is 1",cmd_si}
-
+  //{ "si", "Let the program step N instructions and then pause execution. If N is not given, the default is 1",cmd_si}
   /* TODO: Add more commands */
 
 };
