09d88d38059d0b923e1e184710c0ea773ae6e731
tracer-ics2017 <tracer@njuics.org>
2025-03-17 16:29:29 +0800

    >  compile 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:29:29 up 2 min,  1 user,  load average: 1.31, 0.62, 0.24 d4d671db921cc472d64ca26a9fbd8c229a209ac9

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -306,9 +306,9 @@ uint32_t eval(int p, int q, bool *success) {
       *success = true;
       return strtol(tokens[p].str, NULL, 0);
     }else if (tokens[p].type == TK_REGISTER) {
-      const char *regsl[] = {"eax", "ebx", "ecx", "edx", "esp", "ebp", "esi", "edi"};
-      const char *regsw[] = {"ax", "bx", "cx", "dx", "sp", "bp", "si", "di"};
-      const char *regsb[] = {"al", "bl", "cl", "dl", "ah", "bh", "ch", "dh"};
+      // const char *regsl[] = {"eax", "ebx", "ecx", "edx", "esp", "ebp", "esi", "edi"};
+      // const char *regsw[] = {"ax", "bx", "cx", "dx", "sp", "bp", "si", "di"};
+      // const char *regsb[] = {"al", "bl", "cl", "dl", "ah", "bh", "ch", "dh"};
       
       for (int i = 0; i < 8; i++) {
         if (strcmp(tokens[p].str, regsl[i]) == 0) return reg_l(i);
