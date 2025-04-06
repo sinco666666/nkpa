60a0aaa85daf11b2867595daf4edcdf581f27c6d
tracer-ics2017 <tracer@njuics.org>
2025-04-06 11:09:47 +0800

    >  compile 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:09:47 up 36 min,  1 user,  load average: 0.46, 0.28, 0.27 e3f16d93945a65f4b78b4982d99d6d94d2326e2c

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -54,13 +54,15 @@ make_EHelper(cltd) {
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
     //TODO();
-    rtl_sext(&t0,&cpu.eax,1);
-    cpu.eax = (cpu.eax & 0xffff0000) | (t0 & 0xffff);
+    rtl_lr_b(&t0, R_AL);          
+    rtl_sext(&t0, &t0, 1);        
+    rtl_sr_w(R_AX, &t0);         
   }
   else {
     //TODO();
-    rtl_sext(&t0,&cpu.eax,2);
-    cpu.eax = t0;
+    rtl_lr_w(&t0, R_AX);          
+    rtl_sext(&t0, &t0, 2);        
+    rtl_sr_l(R_EAX, &t0);         
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
