6fb67b1e00141d3be3a7059dd9d5c42b0448c03a
tracer-ics2017 <tracer@njuics.org>
2025-04-09 00:58:15 +0800

    >  compile 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:58:15 up  2:20,  1 user,  load average: 0.32, 0.20, 0.26 e24100ad86d41619c8e09c1fd8591a86dc372ff9

--- nexus-am/apps/microbench/src/queen/queen.c
+++ nexus-am/apps/microbench/src/queen/queen.c
@@ -10,17 +10,7 @@ static unsigned int dfs(unsigned int row, unsigned int ld, unsigned int rd) {
     while (pos) {
       unsigned int p = (pos & (~pos + 1));
       pos -= p;
-      
-      // 限制 ld 和 rd 的值，确保不会产生无效地址
-      unsigned int new_ld = (ld | p) << 1;
-      unsigned int new_rd = (rd | p) >> 1;
-      
-      // 添加额外的安全检查
-      if (new_ld > FULL || new_rd > FULL) {
-        continue; // 跳过可能导致问题的情况
-      }
-      
-      ans += dfs(row | p, new_ld, new_rd);
+      ans += dfs(row | p, (ld | p) << 1, (rd | p) >> 1);
     }
     return ans;
   }
