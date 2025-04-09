f46c03727876c92d603342049b1eb146699679fa
tracer-ics2017 <tracer@njuics.org>
2025-04-09 19:22:09 +0800

    >  compile 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:22:09 up 4 min,  1 user,  load average: 0.19, 0.23, 0.10 20e119c4da99f1c155a726ae95eefd08a1bf85a3

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -1,3 +1,51 @@
+// #include "cpu/exec.h"
+
+// make_EHelper(jmp) {
+//   // the target address is calculated at the decode stage
+//   decoding.is_jmp = 1;
+
+//   print_asm("jmp %x", decoding.jmp_eip);
+// }
+
+// make_EHelper(jcc) {
+//   // the target address is calculated at the decode stage
+//   uint8_t subcode = decoding.opcode & 0xf;
+//   rtl_setcc(&t2, subcode);
+//   decoding.is_jmp = t2;
+
+//   print_asm("j%s %x", get_cc_name(subcode), decoding.jmp_eip);
+// }
+
+// make_EHelper(jmp_rm) {
+//   decoding.jmp_eip = id_dest->val;
+//   decoding.is_jmp = 1;
+
+//   print_asm("jmp *%s", id_dest->str);
+// }
+
+// make_EHelper(call) {
+//   // the target address is calculated at the decode stage
+//   rtl_push(eip);
+//   rtl_addi(&decoding.jmp_eip,eip,id_dest->val);
+//   decoding.is_jmp = 1;
+//   print_asm("call %x", decoding.jmp_eip);
+// }
+
+// make_EHelper(ret) {
+//   rtl_pop(&decoding.jmp_eip);
+//   decoding.is_jmp = 1;
+
+//   print_asm("ret");
+// }
+
+// make_EHelper(call_rm) {
+//   rtl_li(&t2, decoding.seq_eip);
+//   rtl_push(&t2);
+//   decoding.jmp_eip = id_dest->val;
+//   decoding.is_jmp = 1;
+
+//   print_asm("call *%s", id_dest->str);
+// }
 #include "cpu/exec.h"
 
 make_EHelper(jmp) {
@@ -25,14 +73,17 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  rtl_push(eip);
-  rtl_addi(&decoding.jmp_eip,eip,id_dest->val);
+  rtl_li(&t2, decoding.seq_eip);
+  rtl_push(&t2);
+
   decoding.is_jmp = 1;
+
   print_asm("call %x", decoding.jmp_eip);
 }
 
 make_EHelper(ret) {
-  rtl_pop(&decoding.jmp_eip);
+  rtl_pop(&t2);
+  decoding.jmp_eip = t2;
   decoding.is_jmp = 1;
 
   print_asm("ret");
@@ -41,8 +92,7 @@ make_EHelper(ret) {
 make_EHelper(call_rm) {
   rtl_li(&t2, decoding.seq_eip);
   rtl_push(&t2);
-  decoding.jmp_eip = id_dest->val;
+  decoding.jmp_eip = id_dest -> val;
   decoding.is_jmp = 1;
-
   print_asm("call *%s", id_dest->str);
 }
