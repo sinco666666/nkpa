e4d0b6e8cb37278ae5ebe74fafd55be83bbfc51c
tracer-ics2017 <tracer@njuics.org>
2025-04-09 15:51:46 +0800

    >  compile 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:51:46 up 31 min,  1 user,  load average: 1.68, 0.57, 0.27 33e12102727bd7646dc4a39f2af228c6b31a61ea

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -124,30 +124,30 @@ make_rtl_setget_eflags(OF)
 make_rtl_setget_eflags(ZF)
 make_rtl_setget_eflags(SF)
 
-static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
+inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
   // dest <- src1
   *dest = *src1;
 }
 
-inline void rtl_not(rtlreg_t* dest) {
+static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
   *dest = ~(*dest);
 }
 
-inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   int shift = (4 - width) * 8;
   *dest = (int32_t)(*src1 << shift) >> shift;
 }
 
-inline void rtl_push(const rtlreg_t* src1) {
+static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   cpu.esp -= 4;
   vaddr_write(cpu.esp, 4, *src1);
 }
 
-inline void rtl_pop(rtlreg_t* dest) {
+static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
   *dest = vaddr_read(cpu.esp, 4);
