9874045894ac40fa1495e63e0b7bc427964c70fc
tracer-ics2017 <tracer@njuics.org>
2025-04-14 17:23:40 +0800

    >  compile 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:23:40 up 53 min,  1 user,  load average: 0.11, 0.11, 0.09 48cdda0c321e1afde96f6de9f67c26ea413715a

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,12 @@
 #include "common.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  size_t size = get_ramdisk_size();
+  void * buff = NULL;
+  ramdisk_read(buff,0,size);
   return (uintptr_t)DEFAULT_ENTRY;
 }
