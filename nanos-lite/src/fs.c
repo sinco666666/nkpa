94b16a03b2e466f68f1d67fa38364183e1f57768
tracer-ics2017 <tracer@njuics.org>
2025-04-24 20:04:41 +0800

    >  run 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:04:41 up 28 min,  1 user,  load average: 0.23, 0.20, 0.16 23d48ba11b1beabb8576aa376e847df8344e522

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -4,6 +4,7 @@ typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -23,4 +24,32 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+extern void fb_write(const void *buf, off_t offset, size_t len);
+extern void ramdisk_write(const void *buf, off_t offset, size_t len);
+size_t fs_filesz(int fd) {
+	return file_table[fd].size;
+}
+ssize_t fs_write(int fd, const void *buf, size_t len) {
+	ssize_t fs_size = fs_filesz(fd);
+	switch(fd) {
+		case FD_STDOUT:
+		case FD_STDERR:
+			for(int i = 0; i < len; i++) {
+				_putc(((char*)buf)[i]);
+			}
+			break;
+		case FD_FB:
+			fb_write(buf, file_table[fd].open_offset, len);
+			file_table[fd].open_offset += len;
+			break;
+		default:
+			if(file_table[fd].open_offset + len > fs_size)
+				len = fs_size - file_table[fd].open_offset;
+			ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+			file_table[fd].open_offset += len;
+			break;
+	}
+	return len;
 }
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,14 @@
 #include "common.h"
 #include "syscall.h"
 
+ssize_t fs_write(int fd, const void *buf, size_t len);
+static inline _RegSet* sys_write(_RegSet *r){
+  int fd = (int)SYSCALL_ARG2(r);
+  char *buf = (char *)SYSCALL_ARG3(r);
+  int len = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_write(fd,buf,len);
+  return NULL;
+}
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -8,6 +16,7 @@ _RegSet* do_syscall(_RegSet *r) {
   switch (a[0]) {
     case SYS_none: SYSCALL_ARG1(r) =1; break;
     case SYS_exit: _halt(SYSCALL_ARG2(r)); break;
+    case SYS_write:return sys_write(r);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
