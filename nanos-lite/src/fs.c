cbc4206f579969d3d58881a80750fa794662c568
tracer-ics2017 <tracer@njuics.org>
2025-04-27 10:58:57 +0800

    >  run 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:58:57 up  1:13,  1 user,  load average: 0.37, 0.41, 0.29 53e0801f197b7d899ec19af774ec8e717fe5f8b3

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -29,49 +29,52 @@ extern size_t events_read(void *buf, size_t len);
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
-  file_table[FD_FB].size = _screen.height * _screen.width * 4;
+	file_table[FD_FB].size = _screen.height * _screen.width * 4;
 }
 
 size_t fs_filesz(int fd) {
 	return file_table[fd].size;
 }
 
-int fs_open(const char *pathname, int flags, int mode) {
-	//可读写所有文件，故忽略flags mode
-	Log("Pathname: %s", pathname);
-	int i;
-	for (i = 0; i < NR_FILES; i++) {
-		//printf("file name: %s\n", file_table[i].name);
-		if (strcmp(file_table[i].name, pathname) == 0) {
-			return i;
-		}
-	}
-	assert(0);
-	return -1;
+int fs_open(const char* filename, int flags, int mode) {
+  for(int i=0; i<NR_FILES; i++) {
+    if(strcmp(filename, file_table[i].name)==0) {
+      Log("loading %s", filename);
+      file_table[i].open_offset = 0;
+      return i;
+    }
+  }
+  panic("This filename not exist");
+  return -1;
 }
 
 ssize_t fs_read(int fd, void *buf, size_t len) {
 	ssize_t fs_size = fs_filesz(fd);
-	//if(file_table[fd].open_offset >= fs_size) //实际上不会出现这情况
-		//return 0;
-	if (file_table[fd].open_offset + len > fs_size) //偏移量不可以超过文件边界 超出部分舍弃
+	if (file_table[fd].open_offset + len > fs_size)
 		len = fs_size - file_table[fd].open_offset;
 	switch(fd) {
 		case FD_STDOUT:
-		case FD_STDERR:
-		case FD_STDIN:
-			return 0;
+		case FD_FB:
+				break;
 		case FD_EVENTS:
-			len = events_read((void *)buf, len);
-			break;
+				len = events_read((void *)buf, len);
+				break;
 		case FD_DISPINFO:
-			dispinfo_read(buf, file_table[fd].open_offset, len);
-			file_table[fd].open_offset += len;	
-			break;
+				if (file_table[fd].open_offset >= file_table[fd].size)
+						return 0;
+				if (file_table[fd].open_offset + len > file_table[fd].size)
+						len = file_table[fd].size - file_table[fd].open_offset;
+				dispinfo_read(buf, file_table[fd].open_offset, len);
+				file_table[fd].open_offset += len;
+				break;
 		default:
-			ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-			file_table[fd].open_offset += len;
-			break;
+				if(file_table[fd].open_offset >= fs_size || len == 0)
+						return 0;
+				if(file_table[fd].open_offset + len > fs_size)
+						len = fs_size - file_table[fd].open_offset;
+				ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+				file_table[fd].open_offset += len;
+				break;
 	}
 	return len;
 }
@@ -81,54 +84,46 @@ ssize_t fs_write(int fd, const void *buf, size_t len) {
 	switch(fd) {
 		case FD_STDOUT:
 		case FD_STDERR:
-			// call _putc()
-			// 串口已被抽象成stdout stderr
-			for(int i = 0; i < len; i++) {
-				_putc(((char*)buf)[i]);
-			}
-			break;
+				for(int i = 0; i < len; i++) {
+						_putc(((char*)buf)[i]);
+				}
+				break;
 		case FD_FB:
-			// write to frame buffer 显存
-			// device.c:fb_write buff中len字节输出到屏幕上offest处
-			fb_write(buf, file_table[fd].open_offset, len);
-			file_table[fd].open_offset += len;
-			break;
+				fb_write(buf, file_table[fd].open_offset, len);
+				file_table[fd].open_offset += len;
+				break;
 		default:
-			// write to ramdisk
-			//if(file_table[fd].open_offset >= fs_size)
-				//return 0;	
-			if(file_table[fd].open_offset + len > fs_size)
-				len = fs_size - file_table[fd].open_offset;
-			// 对文件的真正读写
-			ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-			file_table[fd].open_offset += len;
-			//Log("offset = %d", file_table[fd].open_offset);
-			break;
+				if(file_table[fd].open_offset >= fs_size)
+						return 0;
+				if(file_table[fd].open_offset + len > fs_size)
+						len = fs_size - file_table[fd].open_offset;
+				ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+				file_table[fd].open_offset += len;
+				break;
 	}
-	return len;// 参见man 返回值
+	return len;
 }
 
 off_t fs_lseek(int fd, off_t offset, int whence) {
 	off_t result = -1;
-	// fs.h
-	// man 2 lseek 同时注意边界问题
 	switch(whence) {
 		case SEEK_SET:
-			if (offset >= 0 && offset <= file_table[fd].size) {
-				file_table[fd].open_offset = offset;
-				result = file_table[fd].open_offset;
-			}
-			break;
+				if (offset >= 0 && offset <= file_table[fd].size) {
+						file_table[fd].open_offset = offset;
+						result = file_table[fd].open_offset = offset;
+				}
+				break;
 		case SEEK_CUR:
-			if ((offset + file_table[fd].open_offset >= 0) && (offset + file_table[fd].open_offset <= file_table[fd].size)) {
-				file_table[fd].open_offset += offset;
-				result = file_table[fd].open_offset;
-			}
-			break;
+				if ((offset + file_table[fd].open_offset >= 0) &&
+						(offset + file_table[fd].open_offset <= file_table[fd].size)) {
+						file_table[fd].open_offset += offset;
+						result = file_table[fd].open_offset;
+				}
+				break;
 		case SEEK_END:
-			file_table[fd].open_offset = file_table[fd].size + offset;
-			result = file_table[fd].open_offset;
-			break;
+				file_table[fd].open_offset = file_table[fd].size + offset;
+				result = file_table[fd].open_offset;
+				break;
 	}
 	
 	return result;
