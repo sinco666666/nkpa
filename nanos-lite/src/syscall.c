49cf6efa11ef39a596879d5407673a97a3f5c675
tracer-ics2017 <tracer@njuics.org>
2025-04-27 10:54:15 +0800

    >  run 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:54:15 up  1:08,  1 user,  load average: 0.18, 0.24, 0.19 1de0c1db5589c4a5c5576e753ff87f81aea66ac

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,15 +10,43 @@ static inline _RegSet* sys_write(_RegSet *r){
   int fd = (int)SYSCALL_ARG2(r);
   char *buf = (char *)SYSCALL_ARG3(r);
   int len = (int)SYSCALL_ARG4(r);
-  Log("");
-  if(fd == 1 || fd == 2){
-      for(int i = 0; i < len; i++) {
-          _putc(buf[i]);
-      }
-      SYSCALL_ARG1(r) = SYSCALL_ARG4(r);
-  }
+  // Log("");
+  // if(fd == 1 || fd == 2){
+  //     for(int i = 0; i < len; i++) {
+  //         _putc(buf[i]);
+  //     }
+  //     SYSCALL_ARG1(r) = SYSCALL_ARG4(r);
+  // }
+  SYSCALL_ARG1(r) = fs_write(fd,buf,len);
   return NULL;
 }
+static inline _RegSet* sys_open(_RegSet *r) {
+  const char* pathname = (const char*)SYSCALL_ARG2(r);
+  int flags = (int)SYSCALL_ARG3(r);
+  int mode = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_open(pathname,flags,mode);
+  return NULL;
+}
+static inline _RegSet* sys_read(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  char *buf = (char *)SYSCALL_ARG3(r);
+  int len = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_read(fd,buf,len);
+  return NULL;
+}
+static inline _RegSet* sys_close(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  SYSCALL_ARG1(r) = fs_close(fd);
+  return NULL;
+}
+static inline _RegSet* sys_lseek(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  off_t offset = (off_t)SYSCALL_ARG3(r);
+  int whence = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_lseek(fd,offset,whence);
+  return NULL;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -28,6 +56,10 @@ _RegSet* do_syscall(_RegSet *r) {
     case SYS_exit: _halt(SYSCALL_ARG2(r)); break;
     case SYS_write: return sys_write(r);
     case SYS_brk:  SYSCALL_ARG1(r) = 0; break;
+    case SYS_open:return sys_open(r);
+    case SYS_read:return sys_read(r);
+    case SYS_close:return sys_close(r);
+    case SYS_lseek:return sys_lseek(r);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
