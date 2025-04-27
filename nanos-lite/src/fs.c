ba4da7fb76add0b4e5222758d9d983d76f6246b6
tracer-ics2017 <tracer@njuics.org>
2025-04-27 22:01:15 +0800

    >  run 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:01:15 up 12:15,  1 user,  load average: 0.28, 0.35, 0.33 47e5b33165c72c2124bb7cc2c6ae7fcb905387

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -58,7 +58,7 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
 		case FD_STDIN:
 			return 0;
 		case FD_EVENTS:
-			len = events_read((void *)buf, len);
+			len = events_read(buf, len);
 			break;
 		case FD_DISPINFO:
 			dispinfo_read(buf, file_table[fd].open_offset, len);
