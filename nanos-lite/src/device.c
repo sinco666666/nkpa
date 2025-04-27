b262798ccf77889c160741d4aa64506783e6a12b
tracer-ics2017 <tracer@njuics.org>
2025-04-27 15:26:40 +0800

    >  run 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:26:40 up  5:41,  1 user,  load average: 0.72, 0.36, 0.20 8aa7ab57204538f1ab1a42f8f119daea3d69b

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -9,7 +9,21 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t len) {
-  return 0;
+  int key = _read_key();
+	bool down = false;
+	if (key & 0x8000) {
+		key ^= 0x8000;
+		down = true;
+	}
+	if (key == _KEY_NONE) {
+		unsigned long t = _uptime();
+		sprintf(buf, "t %d\n", t);
+	}
+	else {
+		sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
+		Log("Get key: %d %s %s\n", key, keyname[key], down ? "down" : "up");
+	}
+	return strlen(buf);
 }
 
 static char dispinfo[128] __attribute__((used));
