28346e5439535c69d3abeea5deb343acc8f3ac0d
tracer-ics2017 <tracer@njuics.org>
2025-04-27 21:55:25 +0800

    >  run 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:55:25 up 12:09,  1 user,  load average: 0.30, 0.33, 0.32 903ee6a3209e31c8e9b14ae9461d0deb852a1e6

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -9,23 +9,24 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t len) {
+  char str[20];
+  bool down = false;
   int key = _read_key();
-	bool down = false;
-	// if (key & 0x8000) {
-	// 	key ^= 0x8000;
-	// 	down = true;
-	// }
-	if (key == _KEY_NONE) {
-		sprintf(buf, "t %d\n", _uptime());
-	}
-	else {
-		if (key & 0x8000) {
-		key ^= 0x8000;
-		down = true;
-	}
-		sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
-	}
-	return strlen(buf);
+  if (key & 0x8000) {
+    key ^= 0x8000;
+    down = true;
+  }
+  if (key != _KEY_NONE)
+    sprintf(str, "%s %s\n", down?"kd":"ku", keyname[key]);
+  else
+    sprintf(str, "t%d\n", _uptime());
+  
+  if (strlen(str) <= len) {
+    strncpy((char*)buf, str, strlen(str));
+    return strlen(str);
+  }
+  Log("strlen(event)>len,return 0");
+  return 0;
 }
 
 static char dispinfo[128] __attribute__((used));
