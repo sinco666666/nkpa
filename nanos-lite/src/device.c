9479b011338e7d836850cc67cb7ced97f200e3e0
tracer-ics2017 <tracer@njuics.org>
2025-04-27 15:16:57 +0800

    >  run 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:16:57 up  5:31,  1 user,  load average: 0.09, 0.12, 0.09 4fe86fc64eb062ce244a1419bb13ec2bf5b56ecb

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,9 +15,13 @@ size_t events_read(void *buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
+  strncpy(buf, dispinfo+offset, len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
+  int row = (offset/4)/_screen.width;
+	int col = (offset/4)%_screen.width;
+	_draw_rect(buf,col,row,len/4,1);
 }
 
 void init_device() {
@@ -25,4 +29,5 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+  sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d\n",_screen.width,_screen.height);
 }
