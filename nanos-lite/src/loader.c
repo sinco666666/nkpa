94e853e7c8265af1ed9d58f7de95abe9e75f3278
tracer-ics2017 <tracer@njuics.org>
2025-04-27 11:04:28 +0800

    >  run 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:04:28 up  1:18,  1 user,  load average: 0.39, 0.31, 0.27 45be9d6432ae69f1ef0077167d64049f9ac55535

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,8 +10,12 @@ ssize_t fs_read(int fd, void *buf, size_t len);
 int fs_close(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  size_t size = get_ramdisk_size();
-  void * buff = DEFAULT_ENTRY;
-  ramdisk_read(buff,0,size);
-  return (uintptr_t)buff;
+  // size_t size = get_ramdisk_size();
+  // void * buff = DEFAULT_ENTRY;
+  // ramdisk_read(buff,0,size);
+  // return (uintptr_t)buff;
+  int fd = fs_open(filename, 0, 0);        //打开文件
+  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd)); //读文件
+  fs_close(fd);                             //关闭文件
+  return (uintptr_t)DEFAULT_ENTRY;
 }
