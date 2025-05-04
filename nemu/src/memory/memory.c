5d8d69e2d612209aa9b4d0512675f49058f7586f
tracer-ics2017 <tracer@njuics.org>
2025-05-04 20:17:01 +0800

    >  compile 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:17:01 up  4:12,  1 user,  load average: 0.32, 0.42, 0.35 f383d5079257b7f1c4bf91ffa41c8f01eef8715a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -54,34 +54,53 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
     mmio_write(addr, len, data, r);
 }
 
+// uint32_t vaddr_read(vaddr_t addr, int len) {
+//   //return paddr_read(addr, len);
+//   //Log("vaddr_read");
+//   if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
+// 	uint32_t data = 0;
+// 	for(int i=0;i<len;i++){
+// 		paddr_t paddr = page_translate(addr + i, false);
+// 		data += (paddr_read(paddr, 1))<<8*i;
+// 	}
+// 	return data;
+//   //assert(0);
+//   } else {
+//     paddr_t paddr = page_translate(addr, false);
+//     return paddr_read(paddr, len);
+//   }
+// }
+
+// void vaddr_write(vaddr_t addr, int len, uint32_t data) {
+//   //Log("vaddr_write");
+//   //paddr_write(addr, len, data);
+//   if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
+// 	for(int i=0;i<len;i++){
+// 		paddr_t paddr = page_translate(addr + i,true);
+// 		paddr_write(paddr,1,data>>8*i);
+// 	}
+//   //assert(0);
+//   } else {
+//     paddr_t paddr = page_translate(addr, true);
+//     paddr_write(paddr, len, data);
+//   }
+// }
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  //return paddr_read(addr, len);
-  //Log("vaddr_read");
-  if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
-	uint32_t data = 0;
-	for(int i=0;i<len;i++){
-		paddr_t paddr = page_translate(addr + i, false);
-		data += (paddr_read(paddr, 1))<<8*i;
-	}
-	return data;
-  //assert(0);
-  } else {
-    paddr_t paddr = page_translate(addr, false);
-    return paddr_read(paddr, len);
-  }
+    if (PTE_ADDR(addr) != PTE_ADDR(addr + len - 1)) {
+        printf("error: the data pass two pages: addr=0x%u, len=%d!\n", addr, len);  // 改为 %u
+        assert(0);
+    } else {
+        paddr_t paddr = page_translate(addr, false);
+        return paddr_read(paddr, len);
+    }
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  //Log("vaddr_write");
-  //paddr_write(addr, len, data);
-  if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
-	for(int i=0;i<len;i++){
-		paddr_t paddr = page_translate(addr + i,true);
-		paddr_write(paddr,1,data>>8*i);
-	}
-  //assert(0);
-  } else {
-    paddr_t paddr = page_translate(addr, true);
-    paddr_write(paddr, len, data);
-  }
+    if (PTE_ADDR(addr) != PTE_ADDR(addr + len - 1)) {
+        printf("error: the data pass two pages: addr=0x%u, len=%d!\n", addr, len);  // 改为 %u
+        assert(0);
+    } else {
+        paddr_t paddr = page_translate(addr, true);
+        paddr_write(paddr, len, data);
+    }
 }
