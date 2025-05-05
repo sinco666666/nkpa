749213edef0845e8877ef70295fd1c9dd0524de8
tracer-ics2017 <tracer@njuics.org>
2025-05-05 20:22:59 +0800

    >  run 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:22:59 up 30 min,  1 user,  load average: 0.20, 0.20, 0.19 b0c92b0f64240ef0242686bec4b3e7eff01f51df

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,6 +4,7 @@ extern _RegSet* do_syscall(_RegSet *r);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:return do_syscall(r);
+    case _EVENT_TRAP:Log("_EVENT_TRAP"); return NULL;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
