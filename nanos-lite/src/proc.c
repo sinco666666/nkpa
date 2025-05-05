3d1bfb3ad11cd470863a54b14184ae140943c660
tracer-ics2017 <tracer@njuics.org>
2025-05-05 20:58:48 +0800

    >  run 2213628 sinco Linux sinco-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:58:48 up  1:06,  1 user,  load average: 0.10, 0.20, 0.20 d51ab358cdc151db4173afa74907db624946612e

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,15 +28,10 @@ void load_prog(const char *filename) {
   pcb[i].tf = _umake(&pcb[i].as, stack, stack, (void *)entry, NULL, NULL);
 }
 
-int count = 0;
 _RegSet* schedule(_RegSet *prev) {
   current->tf = prev;
   //current = &pcb[0];
-  count++;
-  if (count == 1000) {
-    current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-    count = 0;
-  }
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   _switch(&current->as);
   return current->tf;
 }
